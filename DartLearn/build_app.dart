dcd38e3776ca121d9fb79d61019a3f3198ff7706
markovsergey <bestbest@yandex.ru>
2025-03-10 10:18:13 +0300

    Создал build_app

---
+++ DartLearn/build_app.dart
@@ -0,0 +1,7 @@
+// Вызов этого приложения из командной строки
+// dart build_app.dart test_param test_param2
+// dart D:\\DartProjects\\DartLearn1\\DartLearn\\build_app.dart test_param test_param2
+
+void main(List<String> arguments) {
+  print(arguments);
+}
